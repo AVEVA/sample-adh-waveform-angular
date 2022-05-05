68888a6da148101d977944a9434bf269a4f2a013
Anton Dahlin <adahlin@osisoft.com>
2022-05-05 14:47:40 +0200

    switch protractor with cypress

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -1,3 +1,26 @@
+/// <reference types="Cypress" />
+
 // Plugins enable you to tap into, modify, or extend the internal behavior of Cypress
 // For more info, visit https://on.cypress.io/plugins-api
-module.exports = (on, config) => {}
+module.exports = (on, config) => {
+  const path = require('path');
+  const loginProcessHeadless = require(path.resolve(__dirname, './login.js'));
+
+  // This task logs into the portal using a Puppeteer Chrome instance and completes the login process.
+  // After authentication, local storage variables are returned to be copied into the Cypress Chrome instance.
+  on('task', {
+    loginProcessHeadless({
+      portalUrl = config.baseUrl,
+      username,
+      password,
+    }) {
+      return loginProcessHeadless({
+        portalUrl,
+        username,
+        password,
+      });
+    },
+  });
+
+  return config;
+}
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -41,3 +41,40 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+/** @inheritdoc */
+Cypress.Commands.add(
+    'getByTestId',
+    (testId: string) =>
+        cy.get(`[data-testid="${testId}"]`)
+);
+
+/** @inheritdoc */
+Cypress.Commands.add(
+    'clickAndAssert',
+    (testId: string, expectation: string) =>
+        cy.getByTestId(testId).should('be.visible').click().then(() => {
+            cy.getByTestId(testId + 'Message').should('contain.text', expectation)
+        })
+);
+
+/** @inheritdoc */
+Cypress.Commands.add(
+    'loginProcessHeadless', 
+    (username: string, password: string) => 
+        cy.task(
+            'loginProcessHeadless',
+            {
+                username: username,
+                password: password,
+            },
+            { timeout: 300000 },
+        ).then((webStorageData: { localStorageData: Object }) => {
+            console.log(
+                'Copying local storage data with authenticated session information from puppeteer browser, into cypress browser instance'
+            )
+            Object.keys(webStorageData.localStorageData).forEach((key) => {
+                window.localStorage[key] = webStorageData.localStorageData[key];
+            });
+        }));
+
